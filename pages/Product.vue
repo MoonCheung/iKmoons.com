0cc53c04031ea55bf4fbda050bc8d3abe691bd42
MoonCheung <salvador23@163.com>
2019-02-05 02:08:46 +0800

    update code

--- pages/Product.vue
+++ pages/Product.vue
@@ -8,14 +8,14 @@
           這是商品敘述，絕對激安，錯過可惜
       </p>
       <div class="links">
-          <a
-          href="#"
-          target="_blank"
-          class="button--green">加入購物車</a>
-          <a
-          href="#"
-          target="_blank"
-          class="button--grey">立刻購買</a>
+        <a
+        href="#"
+        target="_blank"
+        class="button--green">加入購物車</a>
+        <a
+        href="#"
+        target="_blank"
+        class="button--grey">立刻購買</a>
       </div>
     </div>
   </section>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/">
+      index-page
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return new Promise((resolve)=> {
+      setTimeout(function() {
+        resolve({})
+      },1000)
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  font-size: 20px;
+  text-align: center;
+  padding-top: 100px;
+}
+</style>
