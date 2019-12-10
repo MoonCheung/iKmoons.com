85c91fba1b1e2d041fcb7a82829269dc122fd3ea
MoonCheung <salvador23@163.com>
2019-12-10 13:54:17 +0800

    feat: 粒子背景图

---
+++ plugins/particles.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 粒子效果背景图
+ * @Author: MoonCheung
+ * @Github: https://github.com/MoonCheung
+ * @Date: 2019-12-09 14:37:38
+ * @LastEditors: MoonCheung
+ * @LastEditTime: 2019-12-10 01:32:59
+ */
+
+import particlesJS from 'particles.js';
+
+export default particlesJS;
