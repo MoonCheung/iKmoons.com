051110d2587a6b5ebb77018b78e9f4ac5c6bdf16
MoonCheung <salvador23@163.com>
2020-01-23 16:52:48 +0800

    optimize: server

--- server/index.js
+++ server/index.js
@@ -6,17 +6,14 @@ const { Nuxt, Builder } = require("nuxt");
 const app = new Koa();
 
 // Import and Set Nuxt.js options
-const config = requireESM("../nuxt.config.js");
-config.dev = app.env !== "production";
+const config = requireESM("../nuxt.config")
+config.dev = !(app.env === "production");
 
 async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config);
 
-  const {
-    host = process.env.HOST || "127.0.0.1",
-      port = process.env.PORT || 3000
-  } = nuxt.options.server;
+  const { host, port } = nuxt.options.server;
 
   // Build in development
   if (config.dev) {
