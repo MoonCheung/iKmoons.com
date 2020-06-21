1fe63780c1a25fd281c8fd76cd32c572f8546e1d
MoonCheung <salvador23@163.com>
2020-06-21 15:03:28 +0800

    chore: 移除.prettierrc.jsjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
     /**
@@ -25,42 +23,30 @@ module.exports = {
      * error 或 2 ：表示验证规则，不满足时报错
      */
     // 关闭冲突规则
-    'prettier/prettier': [
-      'off',
-      {
-        printWidth: 100, // 一行的字符数，如果超过会进行换行，默认为80
-        tabWidth: 2, // 一个tab代表几个空格数，默认为80
-        useTabs: false, // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
-        singleQuote: true, //使用单引号而不是双引号
-        semi: true, // 行尾是否使用分号，默认为true
-        trailingComma: 'none', //多行时尽可能打印句号
-        bracketSpacing: true, // 在对象文字中打印括号之间的空格
-        jsxBracketSameLine: true, // 放在最后一行末尾
-        vueIndentScriptAndStyle: true, // 在vue文件允许缩进
-      }
-    ],
+    'prettier/prettier': 'off',
     'object-shorthand': 'off',
     'no-console': 'off',
     'no-undef': 'off',
-    "eqeqeq": "off",
-    "no-unused-vars": "off",
-    "no-extra-boolean-cast": "off",
-    "no-useless-escape": "off",
-    "unicorn/prefer-text-content": "off",
+    eqeqeq: 'off',
+    'no-unused-vars': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-useless-escape': 'off',
+    'unicorn/prefer-text-content': 'off',
     // nuxt 相关规则
     'nuxt/no-cjs-in-config': 'off',
     // vue 相关规则
-    'vue/attributes-order': ['off', {
-      "order": [
-        "OTHER_ATTR"
-      ]
-    }],
-    'vue/order-in-components': ['off', {
-      "order": [
-        "data",
-        "computed",
-      ]
-    }],
+    'vue/attributes-order': [
+      'off',
+      {
+        order: ['OTHER_ATTR']
+      }
+    ],
+    'vue/order-in-components': [
+      'off',
+      {
+        order: ['data', 'computed']
+      }
+    ],
     'import/order': ['off']
   }
-}
+};
