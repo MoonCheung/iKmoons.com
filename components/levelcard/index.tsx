ba09df7f4e3c42068f46361771ffa0c3e2c76abc
MoonCheung <ikmoons.is@gmail.com>
2021-06-10 00:23:02 +0800

    chore: 修改图标props的属性值

--- components/levelcard/index.tsx
+++ components/levelcard/index.tsx
@@ -14,14 +14,14 @@ export default function levelcard({ posts }) {
                   <a className={styles['card-link']} title='link'>
                     <img className={styles['card-img']} src={item.link} />
                     <div className={styles['card-item']}>
-                      {item.name === 'Outdoor' ? (
-                        <Outdoor className={styles['i-icon']} theme='outline' size='36' strokeWidth={3} />
-                      ) : item.name === 'BookOne' ? (
-                        <BookOne className={styles['i-icon']} theme='outline' size='36' strokeWidth={3} />
-                      ) : item.name === 'SettingTwo' ? (
-                        <SettingTwo className={styles['i-icon']} theme='outline' size='36' strokeWidth={3} />
-                      ) : item.name === 'Code' ? (
-                        <Code className={styles['i-icon']} theme='outline' size='36' strokeWidth={3} />
+                      {item.icon === 'Outdoor' ? (
+                        <Outdoor className={styles['i-icon']} theme='outline' size='28' strokeWidth={4} />
+                      ) : item.icon === 'BookOne' ? (
+                        <BookOne className={styles['i-icon']} theme='outline' size='28' strokeWidth={4} />
+                      ) : item.icon === 'SettingTwo' ? (
+                        <SettingTwo className={styles['i-icon']} theme='outline' size='28' strokeWidth={4} />
+                      ) : item.icon === 'Code' ? (
+                        <Code className={styles['i-icon']} theme='outline' size='28' strokeWidth={4} />
                       ) : null}
                       <span className={styles['card-title']}>{item.name}</span>
                     </div>
